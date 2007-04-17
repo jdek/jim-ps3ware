a3dac9ee7aedd4af3bf7790779395fdc8cf8e7a5
warren <warren@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-04-17 09:51:59 +0000

    Initial version of libfb for the PS3 Should facilitate making programs that use the Framebuffer directly

---
+++ libfb/libfb.h
@@ -0,0 +1,61 @@
+/**
+ * PS3 FrameBuffer library
+ * -----------------------
+ * Licensed under the BSD license, see LICENSE for details
+ * 
+ * libfb.h - Interface to the FrameBuffer Library
+ *
+ * Copyright (c) 2007 John Kelley <ps2dev@kelley.ca>
+ *
+ * $Id:
+ */
+
+/**
+  * Initialize the FrameBuffer library and disable the console
+  *
+  * @return address of first frame on success, NULL on failure
+  */
+void *fb_init();
+
+/**
+  * Get the horizontal resolution of the FrameBuffer
+  *
+  * @return int horizontal resolution
+  */
+int fb_getXres();
+
+/**
+  * Get the vertical resolution of the FrameBuffer
+  *
+  * @return int vertical resolution
+  */
+int fb_getYres();
+
+/**
+  * Swap the FrameBuffer immediately
+  *
+  * @return address of the BackBuffer
+  */
+void *fb_swap();
+
+/**
+  * Swap the FrameBuffer after waiting for Vsync
+  *
+  * @return address of the BackBuffer
+  */
+void *fb_swapVsync();
+
+/**
+  * Cleanup FrameBuffer library variables and enable the console
+  *
+  * @return none
+  */
+void fb_cleanup();
+
+/**
+  * Get a textual description of the error when an error condition occurs
+  *
+  * @return description of the error
+  */
+char *fb_getError();
+
