f1d8bc53d5493730975ba0fcf3725fa9b37ed00e
unsolo <unsolo@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-07-07 21:54:32 +0000

    helps to get the right spu_dsputils.h

--- trunk/spu-medialib/spu_utils/spu_dsputils.h
+++ trunk/spu-medialib/spu_utils/spu_dsputils.h
@@ -1,12 +1,47 @@
+/**
+ * SPU-MEDIALIB utility
+ * --------------------------------
+ * Licensed under the BSDv2 
+ *
+ * spu_dsputils.h - SPU digital signal processing utilities
+ *
+ * Copyright (c) 2007, Kristian Jerpetjøn <kristian.jerpetjoen@gmail.com>
+ * $Id:
+ */
+
+// Copyright (c) 2007, Kristian Jerpetjøn <kristian.jerpetjoen@gmail.com>
+
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. The names of the authors may not be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #ifndef __SPU_UTILS_H
 #define __SPU_UTILS_H
 
 #include <spu_mfcio.h>
 #include <math.h>
-#include <spu_print.h>
 #include <spu_matrix.h>
-
-
+#include <spu_extfunct.h>
 
 
 static vector signed short C[3];
@@ -15,25 +50,6 @@ static vector signed short zero;
 
 static vector signed short SQRT_2_S;
 static vector signed short SQRT_1_2_S;
-
-static inline vector signed short spu_smul(vector signed short A, vector signed short B)
-{
-	vector signed int ABe=spu_mule(A,B);
-	vector signed int ABo=spu_mulo(A,B);
-	
-	return (vector signed short)spu_shuffle(spu_rlmaska(ABe,-11),spu_rlmaska(ABo,-11),((vector unsigned char){2,3,18,19,6,7,22,23,10,11,26,27,14,15,30,31}));
-
-};
-
-static inline vector signed short spu_smadd(vector signed short A, vector signed short B,vector signed short C)
-{	
-	// this should be moved inline to avoid stalls....
-	vector signed int ABe=spu_mule(A,B);
-	vector signed int ABo=spu_mulo(A,B);
-	return spu_add((vector signed short)spu_shuffle(spu_rlmaska(ABe,-11),spu_rlmaska(ABo,-11),((vector unsigned char){2,3,18,19,6,7,22,23,10,11,26,27,14,15,30,31})),C);
-
-};
-
 
 
 static inline void IDCT_HALF_S(vector signed short DCT_S[8],vector signed short IDCT_S[8])
@@ -48,14 +64,15 @@ static inline void IDCT_HALF_S(vector signed short DCT_S[8],vector signed short 
 	S13=DCT_S[6];
 	S15=DCT_S[3];
 	S16=DCT_S[5];
+
 	//stage 0
-	printshortvec("input 1",DCT_S[1]);
 	vector signed short T14=spu_smul(SQRT_1_2_S,DCT_S[7]);
 	vector signed short T17=spu_smul(SQRT_1_2_S,DCT_S[1]);
-	printshortvec("T17",T17);
+
 	S14=spu_sub(T17,T14);
 	S17=spu_add(T17,T14);
-	//stage 2 or whatever it is begins
+
+	//stage 2 
 	S25=S15;
 	S26=S16;
 	
@@ -71,7 +88,6 @@ static inline void IDCT_HALF_S(vector signed short DCT_S[8],vector signed short 
 	
 	S22=spu_smadd(spu_sub(zero,S13),S[2],T22);
 	S23=spu_smadd(S13,C[2],T23);
-
 
 	//stage 3
 
@@ -120,7 +136,7 @@ static inline void IDCT_S(vector signed short* block )
 }
 
 
-static inline void init_IDCT_S()
+static inline void init_IDCT_S() // this needs to be initiated prior to execution of idct
 {	
 	short factor=2048;
 	
