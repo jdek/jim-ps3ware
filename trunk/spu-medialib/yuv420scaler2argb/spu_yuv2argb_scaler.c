6a45182a8a8838c77062e9cff5989ec8339bb6ab
unsolo <unsolo@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-09-29 14:31:02 +0000

    fixed some minor bugs and also changed the input parameters to allow for a unplanar representation of yuv420 , where U and V doesnt follow immideately after Y. enjoy

--- trunk/spu-medialib/yuv420scaler2argb/spu_yuv2argb_scaler.c
+++ trunk/spu-medialib/yuv420scaler2argb/spu_yuv2argb_scaler.c
@@ -59,8 +59,8 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 	int tgiu1[2];
 	int tgiv0[2];
 	int tgiv1[2];
-//	int tgi[2];
 	int tgo[2];
+	
 	tgiu1[0]=1;
 	tgiu1[1]=2;
 	tgo[0]=3;
@@ -75,7 +75,6 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 	tgiv0[1]=12;
 	tgiv1[1]=13;
 	tgiv1[1]=14;
-//	int dstsmallcroma=0; N/A
 	
 	int selOut = 0;
 	int selIn = 0;
@@ -94,22 +93,20 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 
 	int first=1;
 
-	unsigned long long Op;//, Yp, Up, Vp;
+	unsigned long long Op;
 	unsigned int msg;
 	unsigned long long YIp,UIp,VIp,YOp;
-//	unsigned long long UOp,VOp;
+
 	int crblock0;
 	int crblock1;
 	int srcsmallcroma=0;
-//	int newlines=0;
+;
 	int noscale=1;
-// 	static	int crblockdst0; // N/A
+
  	static	int crblockdst1;
-//	struct scaler_settings *sc;
-//	
+	static	int crblockdst0;
 	scaler_settings_t sc;
-//	printf("size of struct _t: %d",sizeof(*scaler_settings_t));
-//	sc=(struct scaler_settings_t*)memalign(128,sizeof(*scaler_settings_t));
+
 	dmaGetnWait(iargs,(unsigned int)argp,(int)envp,tag); //getting neccesary data to process image
 	printf("spu_yuv2argb_scaler: SRC width %i,DST width %i\n",iargs->srcW,iargs->dstW);
 	
@@ -127,11 +124,6 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 	int dmacromapos[MAXHEIGHT/2+2];
 	int dmapos[MAXHEIGHT+2];
 	
-//	float weightHfilter1[MAX]
-//	vector float * weightHfilter0=(vector float*)memalign(128,MAXHEIGHT*16); //cmon man u have to fix this its freaking stupid
-//	vector float * weightHfilter1=(vector float*)memalign(128,MAXHEIGHT*16);
-//	vector float * weightHfilter0=(vector float*)memalign(128,MAXHEIGHT*2);
-//	vector float * weightHfilter1=(vector float*)memalign(128,MAXHEIGHT*2);
 	vector float * Ytemp0=(vector float *)memalign(128,MAXWIDTH*4+16);
 	vector float * Ytemp1=(vector float *)memalign(128,MAXWIDTH*4+16);
 	vector float * Utemp=(vector float *)memalign(128,MAXWIDTH*2+16);
@@ -167,8 +159,8 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 	InputV1[1]=(vector unsigned char*)memalign(128,MAXWIDTH/2+16); 	
 
 	vector unsigned char* Output[2];
-	Output[0]=(vector unsigned char*)memalign(128,MAXWIDTH*8+16);	// 1line output
-	Output[1]=(vector unsigned char*)memalign(128,MAXWIDTH*8+16);	// 1line output
+	Output[0]=(vector unsigned char*)memalign(128,MAXWIDTH*8);	// 1line output
+	Output[1]=(vector unsigned char*)memalign(128,MAXWIDTH*8);	// 1line output
 	
 	while (spu_stat_in_mbox() == 0);
 		msg=spu_read_in_mbox();
@@ -187,27 +179,55 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 			crblock0=(iargs->srcW>>1)&~15; // rounded down
 			crblock1=((iargs->srcW>>1) + 15)&~15; //rounded up
 			crblockdst1=((iargs->dstW>>1) + 15)&~15;//destination size rounded up.
+			crblockdst0=((iargs->dstW>>1) + 7)&~7;//destination size rounded up.
+
+			initHFilter(iargs->srcW,iargs->srcH,iargs->dstH,hfilterpos0,hfilterpos1,weightHfilter,dmapos,dmacromapos);
+			
+			for (i=0;i < iargs->dstH;i++)
+			{
+				printf("Hfilterpos0 dst: %d, src:%d, weight:%f\n",i,hfilterpos0[i],weightHfilter[i]);
+				printf("Hfilterpos1 dst: %d, src:%d, weight:%f\n",i,hfilterpos1[i],1.0-weightHfilter[i]);
+			}
+			
 			if ((iargs->srcW==iargs->dstW)&&(iargs->srcH==iargs->dstH))
 			{
-				printf("spu_yuv2argb_scaler: No scaling proceeding with csc\n");
+				printf("spu_yuv2argb_scaler: No scaling proceeding with direct csc\n");
 				noscale=1;
 				
 			} else {
 
 				noscale=0;
-				printf("spu_yuv2argb_scaler: Scaling, computing filters\n");
-				initHFilter(iargs->srcW,iargs->srcH,iargs->dstH,hfilterpos0,hfilterpos1,weightHfilter,dmapos,dmacromapos/*weightHfilter0,weightHfilter1*/);
+				printf("spu_yuv2argb_scaler: Scaling, computing shuffle filters\n");
+
 				initWFilter(iargs->srcW,iargs->dstW,1,wfilterpos,widthfilter0,widthfilter1,weightWfilter0,weightWfilter1);
-				
+
+				for (i=0;i < iargs->dstW/4;i++)
+				{
+					printf("filterpos dst: %d, src:%d\n",i,wfilterpos[i]);
+					printcharvec("widthfilter0",widthfilter0[i]);
+					printcharvec("widthfilter1",widthfilter1[i]);
+					printfvec("weightWfilter0",weightWfilter0[i]);
+					printfvec("weightWfilter1",weightWfilter1[i]);
+				}				
+
 				srcsmallcroma=0;
 				sc.smallcroma=0;
 				if ((iargs->srcW%32) != 0)
 				{
 					sc.smallcroma=1;
 					srcsmallcroma=1;	
-				// we must then compute a separate filter for every other u and v since they arent 128bit alligned.. asuming that it will allways be at least width factor of 16!!!
-					initWcrFilter(iargs->srcW,iargs->dstW,1,crwfilterpos,crwidthfilter0,crwidthfilter1);	
-					printf("spu_yuv2argb_scaler: Computing Crshufflefilter\n");
+					initWcrFilter(iargs->srcW/2,iargs->dstW/2,1,crwfilterpos,crwidthfilter0,crwidthfilter1);	
+					printf("spu_yuv2argb_scaler: Computing Crshuffle filter\n");
+	
+					for (i=0;i < (iargs->dstW>>1)/4;i++)
+					{
+						printf("crwfilterpos dst: %d, src:%d, weight:%f\n",i,crwfilterpos[i]);
+						printcharvec("crwidthfilter0",crwidthfilter0[i]);
+						printcharvec("crwidthfilter1",crwidthfilter1[i]);
+						printfvec("weightWfilter0",weightWfilter0[i]);
+						printfvec("weightWfilter1",weightWfilter1[i]);
+					
+					}
 							
 				}
 				
@@ -219,7 +239,7 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 				sc.crsWfilter0=crwidthfilter0;
 				sc.crsWfilter1=crwidthfilter1;
 				sc.crfilterpos=crwfilterpos;
-				//sc.smallcroma=0;
+
 				sc.smallcromaline=0;
 				
 			}
@@ -232,258 +252,133 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 		UIp = iargs->Ustart[selIn];
 		VIp = iargs->Vstart[selIn];
 		Op = iargs->Output[selOut];
-	//	UOp = YOp + (iargs->dstW*iargs->dstH); //+ iargs->dstW; // fix this to something it should be
-	//	VOp = UOp + (iargs->dstW*iargs->dstH)/4;//*2 + iargs->dstW*1.5;//fix this to something it should be
-		
-		
-	//	int currentpos=0;
-	//	int nextpos=1;
-	//	int current=0;
-	//	int lasthfilterpos=0;	
-		
-		//loop begins
-		LineSelOut=0;
 
-		selY0In=0; //we want to wait for dma 0
+		
+		LineSelOut=0;
+		selY0In=0; 
 		selY1In=0;
 		selCrIn=0;
 
-		if (noscale)
+
+		dmaGet(InputY0[0],YIp+dmapos[hfilterpos0[0]],iargs->srcW,tgiy0[0]); 
+		dmaGet(InputY1[0],YIp+dmapos[hfilterpos1[0]],iargs->srcW,tgiy1[0]); 
+		dmaGet(InputY0[1],YIp+dmapos[hfilterpos0[1]],iargs->srcW,tgiy0[1]); 
+		dmaGet(InputY1[1],YIp+dmapos[hfilterpos1[1]],iargs->srcW,tgiy1[1]); 
+		
+
+		dmaGet(InputU0[0],UIp+dmacromapos[hfilterpos0[0]],crblock1,tgiu0[0]);
+		dmaGet(InputU0[1],UIp+dmacromapos[hfilterpos0[1]],crblock1,tgiu0[1]);
+		dmaGet(InputU1[0],UIp+dmacromapos[hfilterpos1[0]],crblock1,tgiu1[0]);	
+		dmaGet(InputU1[1],UIp+dmacromapos[hfilterpos1[1]],crblock1,tgiu1[1]); 
+
+		dmaGet(InputV0[0],VIp+dmacromapos[hfilterpos0[0]],crblock1,tgiv0[0]);
+		dmaGet(InputV0[1],VIp+dmacromapos[hfilterpos0[1]],crblock1,tgiv0[1]);
+		dmaGet(InputV1[0],VIp+dmacromapos[hfilterpos1[0]],crblock1,tgiv1[0]);	
+		dmaGet(InputV1[1],VIp+dmacromapos[hfilterpos1[1]],crblock1,tgiv1[1]);
+
+
+
+		for (h=0; h < iargs->dstH>>1; h++) //asume that output is allways h/2
 		{
-			dmaGet(InputY0[0],YIp,iargs->srcW,tgiy0[0]); //fetch data for line1 //this is allways pos 0
-			YIp=YIp+iargs->srcW;
+
+			sc.width=iargs->dstW;
+			sc.smallcroma=0;
+
+			sc.wHfilter=weightHfilter[2*h];
+			dmaWaitTag(tgiy0[selY0In]);
+			dmaWaitTag(tgiy1[selY1In]);
+			sc.source00=InputY0[selY0In];
+			sc.source01=InputY1[selY1In];
+			sc.Output=Ytemp0;
 			
-			dmaGet(InputY0[1],YIp,iargs->srcW,tgiy0[1]); //fetch data for line1 
-			YIp=YIp+iargs->srcW;
-			
-			dmaGet(InputU0[0],UIp,crblock1,tgiu0[0]); //fetch data for line1 
-			UIp=UIp+crblock0;
-			
-			dmaGet(InputU0[1],UIp,crblock1,tgiu0[1]); //fetch data for line1 
-			UIp=UIp+crblock1;
-		
-			dmaGet(InputV0[0],VIp,crblock1,tgiv0[0]); //fetch data for line1 
-			VIp=VIp+crblock0;
-			
-			dmaGet(InputV0[1],VIp,crblock1,tgiv0[1]); //fetch data for line1 
-			VIp=VIp+crblock1;
-		
-
-		} else {
- 			dmaGet(InputY0[0],YIp+dmapos[hfilterpos0[0]],iargs->srcW,tgiy0[0]); //fetch data for line1 //this is allways pos 0
- 	
- 			dmaGet(InputY1[0],YIp+dmapos[hfilterpos1[0]],iargs->srcW,tgiy1[0]); //fetch data for line1 
- 	
- 			dmaGet(InputY0[1],YIp+dmapos[hfilterpos0[1]],iargs->srcW,tgiy0[1]); //fetch data for line2 //yes we dont care if we dma in to much
- 	
- 			dmaGet(InputY1[1],YIp+dmapos[hfilterpos1[1]],iargs->srcW,tgiy1[1]); //fetch data for line2 //yes we dont care if we dma in to much
-		
-
-//			if (srcsmallcroma) {
-				//some way to handle this needs to be added here..
-				//unsigned long long SRC0=((hfilterpos0[0]*iargs->srcW)>>1)~15
-			dmaGet(InputU0[0],UIp+dmacromapos[hfilterpos0[0]],crblock1,tgiu0[0]); 			//this is allways pos 0 
-			dmaGet(InputU0[1],UIp+dmacromapos[hfilterpos0[1]],crblock1,tgiu0[1]); 	
-			dmaGet(InputU1[0],UIp+dmacromapos[hfilterpos1[0]],crblock1,tgiu1[0]); 		
-			dmaGet(InputU1[1],UIp+dmacromapos[hfilterpos1[1]],crblock1,tgiu1[1]); 
-	
-			dmaGet(InputV0[0],VIp+dmacromapos[hfilterpos0[0]],crblock1,tgiv0[0]); 			//this is allways pos 0 
-			dmaGet(InputV0[1],VIp+dmacromapos[hfilterpos0[1]],crblock1,tgiv0[1]); 	
-			dmaGet(InputV1[0],VIp+dmacromapos[hfilterpos1[0]],crblock1,tgiv1[0]); 		
-			dmaGet(InputV1[1],VIp+dmacromapos[hfilterpos1[1]],crblock1,tgiv1[1]);
-
-//				dmaGet(InputV0[0],VIp+((hfilterpos0[0]*iargs->srcW)>>1)&~15,crblock1,tgiv0[0]); 			//this is allways pos 0 
-//				dmaGet(InputV0[1],VIp+((hfilterpos0[1]*iargs->srcW)>>1)&~15,crblock1,tgiv0[1]); 	
-//				dmaGet(InputV1[0],VIp+((hfilterpos1[0]*iargs->srcW)>>1)&~15,crblock1,tgiv1[0]); 		
-//				dmaGet(InputV1[1],VIp+((hfilterpos1[1]*iargs->srcW)>>1)&~15,crblock1,tgiv1[1]); 
-//			} else {
-//				dmaGet(InputU0[0],UIp+((hfilterpos0[0]*iargs->srcW)>>1),crblock1,tgiu0[0]); 			//this is allways pos 0 
-//				dmaGet(InputU0[1],UIp+((hfilterpos0[1]*iargs->srcW)>>1),crblock1,tgiu0[1]); 	
-//				dmaGet(InputU1[0],UIp+((hfilterpos1[0]*iargs->srcW)>>1),crblock1,tgiu1[0]); 		
-//				dmaGet(InputU1[1],UIp+((hfilterpos1[1]*iargs->srcW)>>1),crblock1,tgiu1[1]); 
-				
-//				dmaGet(InputV0[0],VIp+((hfilterpos0[0]*iargs->srcW)>>1),crblock1,tgiv0[0]); 			//this is allways pos 0 
-//				dmaGet(InputV0[1],VIp+((hfilterpos0[1]*iargs->srcW)>>1),crblock1,tgiv0[1]); 	
-//				dmaGet(InputV1[0],VIp+((hfilterpos1[0]*iargs->srcW)>>1),crblock1,tgiv1[0]); 		
-//				dmaGet(InputV1[1],VIp+((hfilterpos1[1]*iargs->srcW)>>1),crblock1,tgiv1[1]); 
-//			}
-
-		}
-
-	//	if (srcsmallcroma) {
-			//some way to handle this needs to be added here..
-		//	dmaGet(InputU0[0],UIp+hfilterpos0[0]*iargs->srcW/2,crblock1,tgi[0]); 			//this is allways pos 0 
-		//	dmaGet(InputU1[0],UIp+hfilterpos1[0]*iargs->srcW/2,crblock1,tgi[0]); 		
-
-	//	}else {
-	
-		
-	//	}
-	
-	//	if (srcsmallcroma) {
-			//some way to handle this needs to be added here..
-		//	dmaGet(InputU0[0],UIp+hfilterpos0[0]*iargs->srcW/2,crblock1,tgi[0]); 			//this is allways pos 0 
-		//	dmaGet(InputU1[0],UIp+hfilterpos1[0]*iargs->srcW/2,crblock1,tgi[0]); 		
-
-	/*	} else {
-			dmaGet(InputU0[0],UIp+hfilterpos0[0]*iargs->srcW/2,crblock1,tgi[0]); 			//this is allways pos 0 
-			dmaGet(InputU1[0],UIp+hfilterpos1[0]*iargs->srcW/2,crblock1,tgi[0]); 			 
-			dmaGet(InputU0[1],UIp+hfilterpos0[1]*iargs->srcW/2,crblock1,tgi[1]); 			//this is allways pos 0 
-			dmaGet(InputU1[1],UIp+hfilterpos1[1]*iargs->srcW/2,crblock1,tgi[1]); 	
-
-			dmaGet(InputV0[0],VIp+hfilterpos0[0]*iargs->srcW/2,crblock1,tgi[0]); 			//this is allways pos 0 
-			dmaGet(InputV1[0],VIp+hfilterpos1[0]*iargs->srcW/2,crblock1,tgi[0]); 			 
-			dmaGet(InputV0[1],VIp+hfilterpos0[1]*iargs->srcW/2,crblock1,tgi[1]); 			
-			dmaGet(InputV1[1],VIp+hfilterpos1[1]*iargs->srcW/2,crblock1,tgi[1]); 		
-		
-		}*/
-		//	dmaGet(InputU[0],UOp+
-	//	LineSelIn=0; // want to check the dma at beginning of the loop!
-
-
-		for (h=0; h < iargs->dstH>>1;h++) //asume that output is allways h/2
-		{
-		//	sc.sWfilter0=widthfilter0; 
-		//	sc.sWfilter1=widthfilter1;
-			if (!noscale) { //seems we have to scale
-				sc.width=iargs->dstW;
-				sc.smallcroma=0;
-			//	sc.wHfilter0=weightHfilter0[2*h];
-			//	sc.wHfilter1=weightHfilter1[2*h];
-				sc.wHfilter=weightHfilter[2*h];
-				dmaWaitTag(tgiy0[selY0In]);
-				dmaWaitTag(tgiy1[selY1In]);
-				sc.source00=InputY0[selY0In];
-				sc.source01=InputY1[selY1In];
-				sc.Output=Ytemp0;
-				scale(&sc);									//first Y line scaled
-				dmaGet(InputY0[selY0In],YIp+dmapos[hfilterpos0[2*h+2]],iargs->srcW,tgiy0[selY0In]); 
-				dmaGet(InputY1[selY1In],YIp+dmapos[hfilterpos1[2*h+2]],iargs->srcW,tgiy1[selY1In]); 
-				selY0In=selY0In^1;
-				selY1In=selY1In^1;
-				
-			//	sc.wHfilter0=weightHfilter0[2*h+1];
-			//	sc.wHfilter1=weightHfilter1[2*h+1];
-				sc.wHfilter=weightHfilter[2*h+1];
-				dmaWaitTag(tgiy0[selY0In]);
-				dmaWaitTag(tgiy1[selY0In]);
-				sc.source00=InputY0[selY0In];
-				sc.source01=InputY1[selY0In];
-				sc.Output=Ytemp1;
-				scale(&sc);									//second Y line scaled
-				dmaGet(InputY0[selY0In],YIp+dmapos[hfilterpos0[2*h+3]],iargs->srcW,tgiy0[selY0In]); 
-				dmaGet(InputY1[selY1In],YIp+dmapos[hfilterpos1[2*h+3]],iargs->srcW,tgiy1[selY1In]); 
-				selY0In=selY0In^1;
-				selY1In=selY1In^1;
-			
-				if (srcsmallcroma) //these settings applly for both U and V
-				{	
-					sc.smallcroma=1;
-					if (hfilterpos0[h]&1==1) {
-						sc.smallcromaline=0;	
-					} else if (hfilterpos1[h]&1==1){
-						sc.smallcromaline=1;
-					} else {
-						sc.smallcroma=0; //both lines are 128 bit alligned only when doing extreme downscaling can this happen
-					}
-				}
-				
-			//	sc.width=((iargs->dstW>>1+15)&~15);
-				sc.width=crblockdst1;
-				sc.wHfilter=weightHfilter[h];
-			//	sc.wHfilter0=weightHfilter0[h];			
-			//	sc.wHfilter1=weightHfilter1[h];
-				sc.Output=Utemp;
-		
-				dmaWaitTag(tgiu0[selCrIn]);
-				dmaWaitTag(tgiu1[selCrIn]);
-				sc.source00=InputU0[selCrIn];
-				sc.source01=InputU1[selCrIn];
-				scale(&sc);
-
-				dmaWaitTag(tgiv0[selCrIn]);
-				dmaWaitTag(tgiv1[selCrIn]);
-				sc.Output=Vtemp;
-				sc.source00=InputV0[selCrIn];
-				sc.source01=InputV1[selCrIn];
-				scale(&sc);
-			//	if (srcsmallcroma) {
-					//some way to handle this needs to be added here..
-				//	dmaGet(InputU0[0],UIp+hfilterpos0[0]*iargs->srcW/2,crblock1,tgi[0]); 			//this is allways pos 0 
-				//	dmaGet(InputU1[0],UIp+hfilterpos1[0]*iargs->srcW/2,crblock1,tgi[0]); 		
-		
-			//	}else {
-				dmaGet(InputV0[selCrIn],VIp+dmacromapos[hfilterpos0[h+2]],crblock1,tgiu0[selCrIn]); 		//this is allways pos 0 
-				dmaGet(InputV1[selCrIn],VIp+dmacromapos[hfilterpos1[h+2]],crblock1,tgiu1[selCrIn]); 
-			//	printf("hfilterpos 0 : %d\n",hfilterpos0[h+2]);
-			//	printf("line 0 croma pos : %d\n",dmacromapos[hfilterpos0[h+2]]);			 	
-				dmaGet(InputU0[selCrIn],UIp+dmacromapos[hfilterpos0[h+2]],crblock1,tgiv0[selCrIn]); 		//this is allways pos 0 
-				dmaGet(InputU1[selCrIn],UIp+dmacromapos[hfilterpos1[h+2]],crblock1,tgiv1[selCrIn]); 
-			//	printf("hfilterpos 1 : %d\n",hfilterpos1[h+2]);
-			//	printf("line 1 croma pos : %d\n",dmacromapos[hfilterpos1[h+2]]);
-			//	}
-				selCrIn=selCrIn^1;
-				
-			} else {
- 				dmaWaitTag(tgiy0[selY0In]);
- 				sc.width=iargs->srcW;
- 				sc.source00=InputY0[selY0In];
- 				sc.Output=Ytemp0;
- 				unpack(&sc);
- 				dmaGet(InputY0[selY0In],YIp,iargs->srcW,tgiy0[selY0In]); 
- 				YIp=YIp+iargs->srcW;
- 				selY0In=selY0In^1;	
- 			
- 				dmaWaitTag(tgiy0[selY0In]);
- 				sc.source00=InputY0[selY0In];
- 				sc.Output=Ytemp1;
- 				unpack(&sc);
- 				dmaGet(InputY0[selY0In],YIp,iargs->srcW,tgiy0[selY0In]);
- 				YIp=YIp+iargs->srcW; 
- 				selY0In=selY0In^1;	
-				
-				
-				if (srcsmallcroma) //these settings applly for both U and V
-				{	
-					sc.smallcroma=1;
-					if ((h&1) == 1) {
-						sc.smallcromaline=1;			
-					} else {
-						sc.smallcromaline=0;
-					}
-				} 
-
-				sc.width=crblock1;
-	
-				dmaWaitTag(tgiu0[selCrIn]);
-				sc.source00=InputU0[selCrIn];
-				sc.Output=Utemp;
+			if (noscale) {
 				unpack(&sc);
-				dmaGet(InputU0[selCrIn],UIp,crblock1,tgiu0[selCrIn]);
-	
- 				dmaWaitTag(tgiv0[selCrIn]);
- 				sc.source00=InputV0[selCrIn];
- 				sc.Output=Vtemp;
- 				unpack(&sc);	
- 				dmaGet(InputV0[selCrIn],VIp,crblock1,tgiv0[selCrIn]);
-				
-				if ((h&1) == 1) {
-						UIp=UIp+crblock0;
-						VIp=VIp+crblock0;
-						
-						
-				} else {
-						UIp=UIp+crblock1;
-						VIp=VIp+crblock1;
-				}
-				selCrIn=selCrIn^1;
-				
-				
+			} else {
+				scale(&sc);	
+			}	
+								//first Y line scaled
+			dmaGet(InputY0[selY0In],YIp+dmapos[hfilterpos0[2*h+2]],iargs->srcW,tgiy0[selY0In]); 
+			if (!noscale) { //if we are scaling we also need the second line
+				dmaGet(InputY1[selY1In],YIp+dmapos[hfilterpos1[2*h+2]],iargs->srcW,tgiy1[selY1In]); 
 			}
+			selY0In=selY0In^1;
+			selY1In=selY1In^1;
 			
+
+			sc.wHfilter=weightHfilter[2*h+1];
+			dmaWaitTag(tgiy0[selY0In]);
+			dmaWaitTag(tgiy1[selY0In]);
+			sc.source00=InputY0[selY0In];
+			sc.source01=InputY1[selY0In];
+			sc.Output=Ytemp1;
+			if (noscale) {
+				unpack(&sc);
+			} else {
+				scale(&sc);	
+			}								//second Y line scaled
+			dmaGet(InputY0[selY0In],YIp+dmapos[hfilterpos0[2*h+3]],iargs->srcW,tgiy0[selY0In]); 
+			if(!noscale) { //if we are scaling we also need the second line
+				dmaGet(InputY1[selY1In],YIp+dmapos[hfilterpos1[2*h+3]],iargs->srcW,tgiy1[selY1In]); 
+		
+			}
+			selY0In=selY0In^1;
+			selY1In=selY1In^1;
+		
+			if (srcsmallcroma) //these settings applly for both U and V
+			{	
+				sc.smallcroma=1;
+				if ((hfilterpos0[h]&1)==1) {
+					sc.smallcromaline=0;	
+				} else if ((hfilterpos1[h]&1)==1){
+					sc.smallcromaline=1;
+				} else {
+					sc.smallcroma=0; //both lines are 128 bit alligned only when doing extreme downscaling can this happen
+				}
+			}
+			if (noscale) {
+				sc.width=crblockdst0;//crblockdst1;
+			} else {
+				sc.width=crblockdst0;
+			}
+		//	sc.width=iargs->dstW/2;
+			sc.wHfilter=weightHfilter[h];
+			
+	
+			dmaWaitTag(tgiu0[selCrIn]);
+			dmaWaitTag(tgiu1[selCrIn]);
+			sc.Output=Utemp;
+			sc.source00=InputU0[selCrIn];
+			sc.source01=InputU1[selCrIn];
+			if (noscale) {
+				unpack(&sc);
+			} else {
+				scale(&sc);	
+			}
+
+			dmaWaitTag(tgiv0[selCrIn]);
+			dmaWaitTag(tgiv1[selCrIn]);
+			sc.Output=Vtemp;
+			sc.source00=InputV0[selCrIn];
+			sc.source01=InputV1[selCrIn];
+			if (noscale) {
+				unpack(&sc);
+			} else {
+				scale(&sc);	
+			}
+
+			dmaGet(InputV0[selCrIn],VIp+dmacromapos[hfilterpos0[h+2]],crblock1,tgiu0[selCrIn]); 		//this is allways pos 0 
+			dmaGet(InputU0[selCrIn],UIp+dmacromapos[hfilterpos0[h+2]],crblock1,tgiv0[selCrIn]); 
+			if(!noscale) {	//if we are scaling we also need the second line
+				dmaGet(InputV1[selCrIn],VIp+dmacromapos[hfilterpos1[h+2]],crblock1,tgiu1[selCrIn]);
+				dmaGet(InputU1[selCrIn],UIp+dmacromapos[hfilterpos1[h+2]],crblock1,tgiv1[selCrIn]); 
+			} 
+
+			selCrIn=selCrIn^1;
+							
 			yuv420toARGBfloat(Ytemp0,Ytemp1,Utemp,Vtemp,Output[LineSelOut],iargs->dstW,iargs->maxwidth);
 			dmaPut(Output[LineSelOut],Op,iargs->maxwidth*8,tgo[LineSelOut]);
 			Op=Op+iargs->maxwidth*8;
+			
 			LineSelOut=LineSelOut^1;
 		} 
 	
@@ -507,8 +402,8 @@ int main(unsigned long long speid, unsigned long long argp, unsigned long long e
 			dmaGetnWait(iargs,(unsigned int)argp,(int)envp,tag); //getting neccesary data to process the new image	
 			first=0; // update filters to reflect the new image!
 		}
-		selOut = selOut ^ 1; // flips the output buffers
-		selIn = selIn ^ 1; // flips the input buffers
+		selOut = selOut ^ 1; // flips the output buffer pointers
+		selIn = selIn ^ 1; // flips the input buffer pointers
 	}
 	
 	return 0;
