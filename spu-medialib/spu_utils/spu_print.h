9c028f699b3d59b3d6002efac2a3e9ca95b65a46
unsolo <unsolo@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-10-08 18:31:31 +0000

    fixed a small bug in spu_print.h not detected by gcc-4.3 sorry

--- spu-medialib/spu_utils/spu_print.h
+++ spu-medialib/spu_utils/spu_print.h
@@ -46,13 +46,13 @@ static inline void printfvec(char * name,vector float vf)
 }
 
 
-static inline void printintvec(char * name,vector int vi)
+static inline void printintvec(char * name,vector unsigned int vi)
 {
 	printf("Vector %s=( %d, %d, %d, %d )\n",name, spu_extract(vi,0), spu_extract(vi,1), spu_extract(vi,2), spu_extract(vi,3));
 }
 
 
-static inline void printshortvec(char * name,vector short vs)
+static inline void printshortvec(char * name,vector unsigned short vs)
 {
 	printf("Vector %s=( %d, %d, %d, %d, %d, %d, %d, %d )\n",name, spu_extract(vs,0), spu_extract(vs,1), spu_extract(vs,2), spu_extract(vs,3), spu_extract(vs,4), spu_extract(vs,5), spu_extract(vs,6), spu_extract(vs,7));
 }
