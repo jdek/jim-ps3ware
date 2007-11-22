37829f90e4e1970cd4b1682f7df331f2a3f6c40a
ironpeter <ironpeter@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-11-22 19:51:54 +0000

    dxt textures

---
+++ libps3rsx/tools/tooldxt/compress!!.sh
@@ -0,0 +1,4 @@
+make
+./tooldxt -dxt1 10 ../../art/p512.tga ../../data/p512.dxt1
+./tooldxt -dxt3 10 ../../art/p512.tga ../../data/p512.dxt3
+./tooldxt -dxt5 10 ../../art/p512.tga ../../data/p512.dxt5
