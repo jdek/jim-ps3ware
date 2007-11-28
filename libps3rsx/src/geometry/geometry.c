9f7a2d088f83e07a45e9b400081f7085e7c540fd
ironpeter <ironpeter@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-11-28 20:06:11 +0000

    geometry stuff

---
+++ libps3rsx/src/geometry/geometry.c
@@ -0,0 +1,152 @@
+/*
+ * PS3 GPU blitting test program
+ *
+ * Copyright 2007 Vivien Chappelier <vivien.chappelier@free.fr>
+ * Copyright 2007 Peter Popov <IronSPeter@gmail.com>
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#include "../../include/nouveau_class.h"
+#include "../fifo/utils.h"
+#include "geometry.h"
+
+uint32_t gpu_offset[2] = { 0x0, 0x0d000000 };
+
+void set_xdr_base_address
+(
+    uint32_t gpu_ioif
+)
+{
+	gpu_offset[1] = gpu_ioif;
+}
+
+int set_geometry_source
+(
+    const geom_desc_t *desc,
+    geom_channels_t channel,
+    uint32_t base_offset,
+    uint32_t *fifo,
+    uint32_t hw_sub
+)
+{
+	uint32_t *ptr = fifo;
+	uint32_t Nv3D = hw_sub;
+
+	BEGIN_RING(Nv3D, NV40TCL_VTXFMT( desc->type ), 1 );
+	OUT_RING  ( desc->format | ( desc->components << NV40TCL_VTXFMT_SIZE_SHIFT ) | ( desc->stride << NV40TCL_VTXFMT_STRIDE_SHIFT )  );
+
+	BEGIN_RING(Nv3D, NV40TCL_VTXBUF_ADDRESS( desc->type ), 1 );
+	OUT_RING  ( ( desc->offset + base_offset + gpu_offset[channel] ) | ( channel << 31 ) );
+
+	return ptr - fifo;
+
+}
+
+int set_index_source
+(
+    geom_channels_t channel,
+    uint32_t offset,
+    uint32_t *fifo,
+    uint32_t hw_sub
+)
+{
+	uint32_t *ptr = fifo;
+	uint32_t Nv3D = hw_sub;
+#define NV40TCL_INDEX_ADDRESS 0x181c
+#define NV40TCL_INDEX_CONTROL 0x1820
+#define NV40TCL_INDEX_DATA    0x1824
+
+	BEGIN_RING(Nv3D, NV40TCL_INDEX_ADDRESS, 1 );
+	OUT_RING( ( offset + gpu_offset[channel] ) );
+
+	BEGIN_RING(Nv3D, NV40TCL_INDEX_CONTROL, 1 );
+	OUT_RING( 0x10 | ( channel << 31 ) );
+
+	return ptr - fifo;
+}
+
+int set_geometry_pipe
+(
+    uint32_t ddr_id,
+    uint32_t xdr_id,
+    uint32_t *fifo,
+    uint32_t hw_sub
+)
+{
+	uint32_t *ptr = fifo;
+	uint32_t Nv3D = hw_sub;
+	BEGIN_RING(Nv3D, 0x1dac, 1 );
+	OUT_RING( 0 );
+
+
+	BEGIN_RING(Nv3D,NV40TCL_DMA_VTXBUF0, 2 );
+	OUT_RING  ( ddr_id );
+	OUT_RING  ( xdr_id );
+
+	return ptr - fifo;
+
+}
+
+
+int draw_indexed_primitives
+(
+    primitives_t type,
+    uint32_t first,
+    uint32_t num_primitives,
+    uint32_t *fifo,
+    uint32_t hw_sub
+)
+{
+	uint32_t i;
+	uint32_t *ptr = fifo;
+	uint32_t Nv3D = hw_sub;
+	
+	if( num_primitives == 0 )
+	{
+	    return 0;
+	}
+	
+	BEGIN_RING(Nv3D, NV40TCL_BEGIN_END, 1);
+	OUT_RING  (type);
+
+	uint32_t end = ( num_primitives & 0xff );
+
+	if( end == 0 )
+	{
+	    BEGIN_RING(Nv3D, NV40TCL_INDEX_DATA | 0x40000000, num_primitives / 0x100 );
+	    for( i = 0; i < num_primitives; i += 0x100 )
+	    {
+		OUT_RING( ( first << NV40TCL_VB_VERTEX_BATCH_START_SHIFT ) | ( 0xff << NV40TCL_VB_VERTEX_BATCH_COUNT_SHIFT ) );
+		first += 256;
+	    }    
+	}
+	else
+	{
+	    BEGIN_RING(Nv3D, NV40TCL_INDEX_DATA | 0x40000000, 1 + num_primitives / 0x100 );
+	    for( i = 0; i < num_primitives - end; i += 0x100 )
+	    {
+		OUT_RING( ( first << NV40TCL_VB_VERTEX_BATCH_START_SHIFT ) | ( 0xff << NV40TCL_VB_VERTEX_BATCH_COUNT_SHIFT ) );
+		first += 256;	
+	    }
+	    
+	    OUT_RING( ( first << NV40TCL_VB_VERTEX_BATCH_START_SHIFT ) | ( end  << NV40TCL_VB_VERTEX_BATCH_COUNT_SHIFT ) );
+	
+	}
+	
+	
+
+	BEGIN_RING(Nv3D, NV40TCL_BEGIN_END, 1);
+	OUT_RING  (NV40TCL_BEGIN_END_STOP);
+
+	return ptr - fifo;
+}
+
--- libps3rsx/src/textures/textures.h
+++ libps3rsx/src/textures/textures.h
@@ -5,21 +5,21 @@
  *
  */
 
-typedef enum 
+typedef enum
 {
-DXT1=0x6,
-DXT3=0x7,
-DXT5=0x8
+    DXT1=0x6,
+    DXT3=0x7,
+    DXT5=0x8
 }
 tex_format_t;
 
-typedef struct 
+typedef struct
 {
-uint32_t 	width;
-uint32_t	height;
-uint32_t 	mips;
-uint32_t        length;
-tex_format_t   	format;
+	uint32_t 	width;
+	uint32_t	height;
+	uint32_t 	mips;
+	uint32_t        length;
+	tex_format_t   	format;
 }
 texture_desc_t;
 
@@ -27,35 +27,37 @@ texture_desc_t;
 
 typedef enum
 {
-REPEAT=0x1,
-MIRRORED_REPEAT=0x2,
-CLAMP_TO_EDGE=0x3,
-CLAMP_TO_BORDER=0x4,
-CLAMP=0x5,
-MIRROR_CLAMP_TO_EDGE=0x6,
-MIRROR_CLAMP_TO_BORDER=0x7,
-MIRROR_CLAMP=0x8
+    REPEAT=0x1,
+    MIRRORED_REPEAT=0x2,
+    CLAMP_TO_EDGE=0x3,
+    CLAMP_TO_BORDER=0x4,
+    CLAMP=0x5,
+    MIRROR_CLAMP_TO_EDGE=0x6,
+    MIRROR_CLAMP_TO_BORDER=0x7,
+    MIRROR_CLAMP=0x8
 }
 tex_wrap_t;
 
-typedef enum 
+typedef enum
 {
-NEAREST=0x1,
-LINEAR=0x2,
-NEAREST_MIPMAP_NEAREST=0x3,
-LINEAR_MIPMAP_NEAREST=0x4,
-NEAREST_MIPMAP_LINEAR=0x5,
-LINEAR_MIPMAP_LINEAR=0x6
+    NEAREST=0x1,
+    LINEAR=0x2,
+    NEAREST_MIPMAP_NEAREST=0x3,
+    LINEAR_MIPMAP_NEAREST=0x4,
+    NEAREST_MIPMAP_LINEAR=0x5,
+    LINEAR_MIPMAP_LINEAR=0x6
 }
 tex_filter_t;
 
-int set_texture_2D( 
-const texture_desc_t *desc, 
-uint32_t offset, 
-uint32_t unit, 
-tex_wrap_t wrap, 
-tex_filter_t mag_filter,
-tex_filter_t min_filter,
-uint32_t *fifo_buffer, 
-uint32_t hw_sub );
+int set_texture_2D
+(
+    const texture_desc_t *desc,
+    uint32_t offset,
+    uint32_t unit,
+    tex_wrap_t wrap,
+    tex_filter_t mag_filter,
+    tex_filter_t min_filter,
+    uint32_t *fifo_buffer,
+    uint32_t hw_sub 
+);
 
