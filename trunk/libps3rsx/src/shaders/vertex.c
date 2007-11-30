aa2ce72fbfc747b6ccd0397ef36dd6e7af06186c
ironpeter <ironpeter@60a87dbf-8814-0410-b77e-0f20dd5992a5>
2007-11-30 20:24:50 +0000

    shader load

---
+++ trunk/libps3rsx/src/shaders/vertex.c
@@ -0,0 +1,66 @@
+/*
+ * PS3 GPU blitting test program
+ *
+ * Copyright 2007 Vivien Chappelier <vivien.chappelier@free.fr>
+ * Copyright 2007 Peter Popov <IronSPeter@gmail.com>
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#include "../../include/nouveau_class.h"
+#include "../fifo/utils.h"
+#include "vertex.h"
+
+int set_vertex_shader
+(
+    const vertex_shader_desc_t *desc,
+    const uint32_t *data_storage,
+    uint32_t *fifo,
+    uint32_t hw_subch 
+)
+{
+	uint32_t i;
+	uint32_t *ptr = fifo;
+	uint32_t Nv3D = hw_subch;
+	
+	BEGIN_RING(Nv3D, NV40TCL_VP_UPLOAD_FROM_ID, 1);
+	OUT_RING  (0);
+	
+	for ( i=0; i<desc->dword_length; i+=4 )
+	{
+		BEGIN_RING(Nv3D, NV40TCL_VP_UPLOAD_INST(0), 4);
+		OUT_RING  (data_storage[i + 0]);
+		OUT_RING  (data_storage[i + 1]);
+		OUT_RING  (data_storage[i + 2]);
+		OUT_RING  (data_storage[i + 3]);
+		
+		//printf( "%x %x %x %x \n", data_storage[i + 0], data_storage[i + 1],  data_storage[i + 2],  data_storage[i + 3] );
+
+	}
+
+
+	BEGIN_RING(Nv3D, NV40TCL_VP_START_FROM_ID, 1);
+	OUT_RING  (0);
+
+	BEGIN_RING(Nv3D, NV40TCL_VP_ATTRIB_EN, 2);
+	OUT_RING  ( desc->vp_in );
+	OUT_RING  ( desc->vp_out );
+
+	BEGIN_RING( Nv3D, 0x1478, 1 );
+	OUT_RING  (0);
+	
+	//printf( "%x \n", desc->vp_in );
+	//printf( "%x \n", desc->vp_out );
+	
+
+	return ptr - fifo;
+}
+
---
+++ trunk/libps3rsx/src/shaders/vertex.h
@@ -0,0 +1,25 @@
+/*
+ * PS3 GPU blitting test program
+ *
+ * Copyright 2007 Peter Popov <IronSPeter@gmail.com>
+ *
+ */
+
+typedef struct
+{
+	uint32_t 	vp_in;
+	uint32_t	vp_out;
+	uint32_t 	dword_length;
+	uint32_t	aux;
+}
+vertex_shader_desc_t;
+
+
+int set_vertex_shader
+(
+    const vertex_shader_desc_t *desc,
+    const uint32_t *data_storage,
+    uint32_t *fifo_buffer,
+    uint32_t hw_sub 
+);
+
